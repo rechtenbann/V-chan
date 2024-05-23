fd6580d96e0e66182f4e8761d0811cecf2334433
sdmatayoshi <sdmatayoshi@gmail.com>
2024-05-22 21:54:17 -0300

    fix menu error closing on option = 2

--- views/home.php
+++ views/home.php
@@ -2,7 +2,7 @@
 require_once "includes/config.php";
 ?>
 <div align="center">
-<h1 style="font-size: 50px;<?php if($_COOKIE['dark_mode']=='true'){echo "color: white;";} ?>"><b><u>V-Chan</u></b></h1>
+<h1 style="font-size: 50px;<?php if(isset($_COOKIE['dark_mode'])&&$_COOKIE['dark_mode']=='true'){echo "color: white;";} ?>"><b><u>V-Chan</u></b></h1>
 <br>
 <a href="posts.php?pag=1&tag=1"><b>All posts</b></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="crear.php">Forum</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="crear.php">Wiki</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="settings.php">Settings</a>
 <br>
@@ -10,4 +10,4 @@ require_once "includes/config.php";
 <input type="text" placeholder="Browse:..." style="width:500px">&nbsp;<input type="submit" value="Search" style="width:100px">
 </form>
 </div>
-<?php for($i=0;$i<10;$i++){echo "<br>";}?>
+<?php for($i=0;$i<24;$i++){echo "<br>";}?>
