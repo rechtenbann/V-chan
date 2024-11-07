bb1282ada29e438bfd23559c733ca0194fe9b128
Lean <matigom13@gmail.com>
2024-11-07 13:00:58 -0300

    Diseño de login y signup

--- views/login.php
+++ views/login.php
@@ -1,28 +1,29 @@
+<link rel="stylesheet" href="css/form.css">
 
-<div >
-<form method="POST">
-        <div>
-            <label for="login_username">
-                <span class="hidden">Username</span>
-            </label>
-            <input autocomplete="username" id="login__username" type="text" name="nombre" class="form__input" placeholder="Nombre de usuario" required>
-        </div>
-        <div>
-            <label for="login__password">
-                <span class="hidden">Password</span>
-            </label>
-            <input id="login__password" type="password" name="contra" class="form__input" placeholder="Contraseña" required>
-        </div>
-        <input type="checkbox" name="cookie"> Mantener sesión iniciada
-        <div>
-            <input type="submit" value="Iniciar Sesion">
-        </div>
-    </form>
-        <p class="text--center">¿No tienes cuenta? <a href="signup.php"  style="color:#212121;">Registrate</a>
-            <svg class="icon">
+<div align="center">
+    <div class="containerForm">
+        <h3>Log in</h3>
+        <form method="POST">
+            <div>
+                <label for="login_username"> Username </label>
+                <input autocomplete="username" id="login__username" type="text" name="nombre" class="form__input inputTextForm" placeholder="Nombre de usuario" required>
+            </div>
+            <div style="margin-top:20px; margin-bottom:20px;">
+                <label for="login__password">Password</label>
+                <input id="login__password" type="password" name="contra" class="form__input inputTextForm" placeholder="Contraseña" required>
+            </div>
+            <div style="font-family:AnimeAce; margin-bottom:10px;">
+                <input type="checkbox" name="cookie"> Mantener sesión iniciada
+            </div>      
+            <div>
+                <input type="submit" value="Iniciar Sesion" class="inputSubmitForm">
+            </div>
+        </form>
+        <p class="text--center" style="font-family:AnimeAce;">¿No tienes cuenta? <a href="signup.php" style="color:#212121; text-decoration:none;">Registrate</a>
+            <!-- <svg class="icon">
                 <use xlink:href="#icon-arrow-right"></use>
-            </svg>
+            </svg> -->
         </p>
     </div>
-    </div>
+</div>
     
--- views/signup.php
+++ views/signup.php
@@ -1,34 +1,30 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha384-ZM+Igh7tGha7ZoD/eSJK2FAzWy0Z6q95u7XN2ogb+NLKpZvgTqTcBE8t+/xDSm/C" crossorigin="anonymous">
+<link rel="stylesheet" href="css/form.css">
 
-    <div >
+<div align="center">
+    <div class="containerForm">
+        <h3>Signup</h3>
         <form method="POST" >
-            <div>
-                <label for="login__username">
-                    <span class="hidden">Username</span>
-                </label>
-                <input autocomplete="username" id="login__username" type="text" name="nombre" class="form__input" placeholder="Nombre de usuario" required>
+            <div style="margin-bottom:20px;">
+                <label for="login__username">Username</label>
+                <input autocomplete="username" id="login__username" type="text" name="nombre" class="form__input inputTextForm" placeholder="Nombre de usuario" required>
             </div>
-            <div >
-                <label for="login__username">
-                    <span class="hidden">E-mail</span>
-                </label>
-                <input autocomplete="username" id="login__username" type="text" name="email" class="form__input" placeholder="Nombre de usuario" required>
+            <div style="margin-bottom:20px;">
+                <label for="login__username">E-mail</label>
+                <input autocomplete="username" id="login__username" type="text" name="email" class="form__input inputTextForm" placeholder="E-mail del usuario" required>
             </div>
-            <div >
-                <label for="login__password">
-                    <span class="hidden">Password</span>
-                </label>
-                <input id="login__password" type="password" name="contra" class="form__input" placeholder="Contraseña" required>
+            <div style="margin-bottom:20px;">
+                <label for="login__password">Password</label>
+                <input id="login__password" type="password" name="contra" class="form__input inputTextForm" placeholder="Contraseña" required>
             </div>
-            <div >
-                <input type="submit" value="Iniciar Sesion">
+            <div style="margin-top:10px;">
+                <input type="submit" value="Iniciar Sesion" class="inputSubmitForm">
             </div>
         </form>
-        <p class="text--center">¡Ya tienes una cuenta? <a href="login.php"  style="color:#212121;">Inicia sesión</a>
-            <svg class="icon">
+        <p class="text--center" style="font-family:AnimeAce;">¿Ya tienes una cuenta? <a href="login.php"  style="color:#212121; text-decoration:none;">Inicia sesión</a>
+            <!-- <svg class="icon">
                 <use xlink:href="#icon-arrow-right"></use>
-            </svg>
+            </svg> -->
         </p>
     </div>
-    </div>
-    
+</div>
