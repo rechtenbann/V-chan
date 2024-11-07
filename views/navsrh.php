f8b82484c99aec585d01084c08be9f51a21fa233
Lean <matigom13@gmail.com>
2024-11-07 14:04:18 -0300

    Diseño de navsrh

--- views/navsrh.php
+++ views/navsrh.php
@@ -7,7 +7,7 @@ if (!isset($section)) {
 <nav class="navsrh">
 <div>
   <form action="result_search.php" method="GET">
-    <input type="text" name="query" style="padding: 7px; width: calc(100% - 180px); border: 1px solid #e0e0e0;" placeholder="Buscar usuarios (@), tags (#) o foro (/)" class="inputTextHome">
+    <input type="text" name="query" style="width: calc(100% - 200px);" placeholder="Buscar usuarios (@), tags (#) o foro (/)" class="inputTextHome">
     <button type="submit" class="inputSubmitHome">Buscar</button>
 </form>
 </div>
